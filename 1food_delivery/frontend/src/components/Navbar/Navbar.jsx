ae9490939411bd125247b1a4243691e8351486df
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-08-28 23:17:49 +0530

    added navbar

--- 1food_delivery/frontend/src/components/Navbar/Navbar.jsx
+++ 1food_delivery/frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,35 @@
+import { admin_assets } from '../../assets/admin_assets/admin_assets.js'
+import { frontend_assets } from '../../assets/frontend_assets/frontend_assets.js'
 import './Navbar.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        
-    </div>
-  )
+
+    const [menu, setMenu] = useState("mobile-app");
+
+    return (
+        <div className='navbar'>
+            <img src={frontend_assets.logo} alt="Loading" />
+            <ul className="navbar-menu">
+                <li onClick={() => { setMenu("home") }} className={menu === "home" ? "active" : ""}>home</li>
+                <li onClick={() => { setMenu("menu") }} className={menu === "menu" ? "active" : ""}>menu</li>
+                <li onClick={() => { setMenu("mobile-app") }} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
+                <li onClick={() => { setMenu("contact-app") }} className={menu === "contact-app" ? "active" : ""}>contact-app</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={frontend_assets.search_icon} alt="Loading" />
+                <div className='navbar-search-icon'>
+                    <img src={frontend_assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+
+                </div>
+                <button>Sign in</button>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
+
+
+
