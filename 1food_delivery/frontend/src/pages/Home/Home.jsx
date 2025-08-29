c1db414531d0bebc64c3a69d398ca96119572843
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-08-29 22:27:38 +0530

    added ExploreMenu and some extra files

--- 1food_delivery/frontend/src/pages/Home/Home.jsx
+++ 1food_delivery/frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,16 @@
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx"
 import Header from "../../components/Header/Header.jsx"
 import './home.css'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Home = () => {
+
+  const [category,setCategory]=useState("All")
+
   return (
     <div>
         <Header/>
+        <ExploreMenu category={setCategory} setCategory={setCategory}/>
     </div>
   )
 }
