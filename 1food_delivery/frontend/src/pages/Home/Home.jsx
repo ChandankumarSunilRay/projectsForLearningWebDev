63af094a485a842fd583ed2fe486fc7fe2d1a6a6
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-08-30 13:27:53 +0530

    added FoodItem folder and under that FoodItem.css and .jsx

--- 1food_delivery/frontend/src/pages/Home/Home.jsx
+++ 1food_delivery/frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,18 @@
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu.jsx"
+import FoodDisplay from "../../components/FoodDisplay/FoodDisplay.jsx"
 import Header from "../../components/Header/Header.jsx"
 import './home.css'
 import React, { useState } from 'react'
 
 const Home = () => {
 
-  const [category,setCategory]=useState("All")
+  const [category, setCategory] = useState("All")
 
   return (
     <div>
-        <Header/>
-        <ExploreMenu category={category} setCategory={setCategory}/>
-
+      <Header />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category = {category}/>
     </div>
   )
 }
