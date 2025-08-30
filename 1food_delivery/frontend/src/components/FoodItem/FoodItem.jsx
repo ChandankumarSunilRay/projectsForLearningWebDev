281a01e07b233eaf7be3a5937403a95457d6b234
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-08-30 17:09:37 +0530

    FoodItem.css and FoodDisplay.css has been completed

--- 1food_delivery/frontend/src/components/FoodItem/FoodItem.jsx
+++ 1food_delivery/frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import './FoodItem.css'
+import { frontend_assets } from '../../assets/frontend_assets/frontend_assets'
 
-
-const FoodItem = () => {
-return    
-  (
-    <div>FoodItem</div>
-  )
+const FoodItem = ({ id, name, price, description, image }) => {
+    return (
+        <div className="food-item">
+            <div className="food-item-image-container">
+                <img className='food-item-image' src={image} alt="loading" />
+            </div>
+            <div className="food-item-info">
+                <div className="food-item-name-rating">
+                    <p>{name}</p>
+                    <img src={frontend_assets.rating_starts} alt="loading" />
+                </div>
+                <p className="food-item-desc">{description}</p>
+                <p className="food-item-price">₹{price}</p>
+            </div>
+        </div>
+    )
 }
-
 
 export default FoodItem
