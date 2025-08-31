aac8197a63f08e08568bd4525a99e9817564cd9a
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-01 00:44:05 +0530

    made reponsive landing page

--- 1food_delivery/frontend/src/components/Navbar/Navbar.jsx
+++ 1food_delivery/frontend/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import { admin_assets } from '../../assets/admin_assets/admin_assets.js'
 import { frontend_assets } from '../../assets/frontend_assets/frontend_assets.js'
 import './Navbar.css'
 import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
 
 const Navbar = () => {
 
@@ -11,10 +12,10 @@ const Navbar = () => {
         <div className='navbar'>
             <img src={frontend_assets.logo} alt="Loading" />
             <ul className="navbar-menu">
-                <li onClick={() => { setMenu("home") }} className={menu === "home" ? "active" : ""}>home</li>
-                <li onClick={() => { setMenu("menu") }} className={menu === "menu" ? "active" : ""}>menu</li>
-                <li onClick={() => { setMenu("mobile-app") }} className={menu === "mobile-app" ? "active" : ""}>mobile-app</li>
-                <li onClick={() => { setMenu("contact-app") }} className={menu === "contact-app" ? "active" : ""}>contact-app</li>
+                <Link to='/' onClick={() => { setMenu("home") }} className={menu === "home" ? "active" : ""}>home</Link>
+                <a href='#explore-menu' onClick={() => { setMenu("menu") }} className={menu === "menu" ? "active" : ""}>menu</a>
+                <a href='#app-download' onClick={() => { setMenu("mobile-app") }} className={menu === "mobile-app" ? "active" : ""}>mobile-app</a>
+                <a href='#footer' onClick={() => { setMenu("contact-app") }} className={menu === "contact-app" ? "active" : ""}>contact-app</a>
             </ul>
             <div className="navbar-right">
                 <img src={frontend_assets.search_icon} alt="Loading" />
