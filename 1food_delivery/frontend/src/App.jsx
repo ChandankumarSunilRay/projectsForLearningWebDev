ef1e22a5bf9362294539276a0f73b8322fa4a11d
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-08-31 23:31:28 +0530

    Added Footer comtent at Footer.jsx

--- 1food_delivery/frontend/src/App.jsx
+++ 1food_delivery/frontend/src/App.jsx
@@ -4,20 +4,24 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Placeorder from './pages/Placeorder/Placeorder'
+import Footer from './components/footer/Footer'
 
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/>
+    <>
+      <div className='app'>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
           <Route path='/order' element={<Placeorder />} />
         </Routes>
-      
 
-    </div>
+      </div>
+      <Footer />
+    </>
+
 
   )
 }
--- 1food_delivery/frontend/src/components/footer/Footer.jsx
+++ 1food_delivery/frontend/src/components/footer/Footer.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import './Footer.css'
+import { frontend_assets } from '../../assets/frontend_assets/frontend_assets'
 
 
 const Footer = () => {
-  return (
-    <div className='footer'>
-        
-    </div>
-  )
+    return (
+        <div className='footer' id='footer'>
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={frontend_assets.logo} alt="loading" />
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias quibusdam culpa fugit eligendi ullam nulla!</p>
+                    <div className="footer-social-icon">
+                        <img src={frontend_assets.facebook_icon} alt="loading" />
+                        <img src={frontend_assets.twitter_icon} alt="loading" />
+                        <img src={frontend_assets.linkedin_icon} alt="loading" />
+                    </div>
+                </div>
+                <div className="footer-content-right">
+                    <h2>COMANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+91 9987521999</li>
+                        <li>contact@tomato.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className='footer-copyright'>Copyright 2024 Tomato.com -All Rights Reserved </p>
+        </div> 
+
+    )
 }
 
 export default Footer
