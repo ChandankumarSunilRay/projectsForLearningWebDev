da1e6fd5e6b0a355e21f8c8d87d7841aea07cf91
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-04 00:52:27 +0530

    added structure of Add.jsx

--- 1food_delivery/admin/src/App.jsx
+++ 1food_delivery/admin/src/App.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/sidebar/sidebar.jsx'
+import {Routes,Route} from 'react-router-dom'
+import Add from './pages/Add/Add.jsx'
+import List from './pages/List/List.jsx'
+import { Orders } from './pages/Order/Orders.jsx'
+
 
 const App = () => {
   return (
@@ -9,6 +14,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path="/Add" element={<Add/>}/>
+          <Route path="/List" element={<List/>}/>
+          <Route path="/Orders" element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
--- 1food_delivery/admin/src/pages/Order/Order.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import './Order.css'
-
-export const Order = () => {
-  return (
-    <div>Order</div>
-  )
-}
