5983f0194d3ca8bca265e0ee699980075210a762
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-03 23:33:39 +0530

    navigation bar has been added

--- 1food_delivery/admin/src/App.jsx
+++ 1food_delivery/admin/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar.jsx'
-import Sidebar from './components/Sidebar/Sidebar.jsx'
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/sidebar/sidebar.jsx'
 
 const App = () => {
   return (
--- 1food_delivery/admin/src/components/Navbar/Navbar.jsx
+++ 1food_delivery/admin/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import "./Navbar.css"
+import './Navbar.css'
+import { assets } from "../../assets/assets.js";
+
 
 
 const Navbar = () => {
-    
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <img className='logo' src={assets.logo} alt="loading" />
+      <img className='profile' src={assets.profile_image} alt="loading" />
+      
+    </div>
   )
 }
-
 
 export default Navbar
