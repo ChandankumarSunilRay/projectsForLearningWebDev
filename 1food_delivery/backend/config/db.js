0e9ec5d0a215436592cb66f9ebf1dd6062a5cf01
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-05 00:15:58 +0530

    login api has been successfully made previously register api was made

--- 1food_delivery/backend/config/db.js
+++ 1food_delivery/backend/config/db.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose"
 
 export const connectDB = async () => {
+  await mongoose.connect('mongodb+srv://chandan128902:CL6Qw3XmaPgGTtWU@clusterone.qc9gftw.mongodb.net/food-del').then(() => console.log("DB Connected"))
 
 }
 
