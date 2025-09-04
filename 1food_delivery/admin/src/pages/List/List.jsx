01d5addfc2d4a3bad991b6423c8e95e0a6c6727b
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-04 14:54:13 +0530

    completed admin/List.jsx struture next step is its .css

--- 1food_delivery/admin/src/pages/List/List.jsx
+++ 1food_delivery/admin/src/pages/List/List.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './List.css'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const List = () => {
+
+  const url = "http://localhost:4000"
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`)
+    console.log(response.data);
+    if (response.data.success) {
+      setList(response.data.data)
+
+    } else {
+      toast.error("Error")
+    }
+  }
+
+  useEffect(() => {
+    fetchList();
+  }, [])
+
   return (
-    <div>List</div>
+    <div className='list add flex-col'>
+      <p>All Food List</p>
+      <div className='list-table'>
+        <div className='list-table-formate title'>
+          <b>Image</b><b>Name</b><b>Catgory</b><b>prise</b><b>Action</b>
+        </div>
+        {list.map((item, index) => {
+          return (
+            <div key={index} className='list-table-formate'>
+              <img src={`${url}/images/` + item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.category}</p>
+              <p>₹{item.price}</p>
+              <p>X</p>
+
+            </div>
+          )
+        })}
+      </div>
+    </div>
+
   )
 }
 
