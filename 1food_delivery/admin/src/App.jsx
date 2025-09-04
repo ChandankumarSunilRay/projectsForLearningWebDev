6a21a4ff52eafdfb50e49c6df7f3b4fe7c6b9dab
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-04 15:33:17 +0530

    in two days i'll working on controllers , models and routes

--- 1food_delivery/admin/src/App.jsx
+++ 1food_delivery/admin/src/App.jsx
@@ -5,24 +5,25 @@ import { Routes, Route } from 'react-router-dom'
 import Add from './pages/Add/Add.jsx'
 import List from './pages/List/List.jsx'
 import { Orders } from './pages/Order/Orders.jsx'
-
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 
 
 const App = () => {
+
+  const url = 'http://localhost:4000'
   return (
     <div>
-      <ToastContainer/>
+      <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
-          <Route path="/Add" element={<Add />} />
-          <Route path="/List" element={<List />} />
-          <Route path="/Orders" element={<Orders />} />
+          <Route path="/Add" element={<Add  url={url}/>} />
+          <Route path="/List" element={<List  url={url}/>} />
+          <Route path="/Orders" element={<Orders  url={url}/>} />
         </Routes>
       </div>
     </div>
--- 1food_delivery/backend/config/db.js
+++ 1food_delivery/backend/config/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose"
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://chandan128902:CL6Qw3XmaPgGTtWU@clusterone.qc9gftw.mongodb.net/food-del').then(()=>console.log("DB Connected"))
 
 }
 
