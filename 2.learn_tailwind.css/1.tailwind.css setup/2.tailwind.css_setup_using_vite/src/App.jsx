3fda68d710c9f0f14616c8a39e7e5e4cbbcba969
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-08 21:22:17 +0530

    tailwind setup completed

---
+++ 2.learn_tailwind.css/1.tailwind.css setup/2.tailwind.css_setup_using_vite/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className='bg-red-300'>This is Chandnkumar Ray</h1>,
+      <h1 className='bg-red-300'>This is second tag</h1>
+    </div>
+  )
+}
+
+export default App
