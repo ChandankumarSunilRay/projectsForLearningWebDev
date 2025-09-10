cc4ce5cb85f7c1a8854aae45e9765dcd9591f423
ChandankumarSunilRay <chandankumarsunilray@gmail.com>
2025-09-10 22:55:51 +0530

    setup has been done for upcomming project

--- 2.learn_tailwind.css/3lecture/tailwind.config.js
+++ 2.learn_tailwind.css/3lecture/tailwind.config.js
@@ -12,7 +12,25 @@
 module.exports = {
   content: ["./index.html", "./**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#2042a",
+        lightBlue:"#2b84ea",
+        lightBlue300:"#4b94ed",
+        lightBlue500:"#0b72e7",
+        greenLight:"#61cea6",
+        greyText:"#818597",
+        lightGrey:"#e2e2e2",
+        greyBlue:"#344a6c",
+        deepBlueHead:"#162f56",
+        grey2:"#525a76",
+
+
+      }
+},
   },
   plugins: [],
 };
