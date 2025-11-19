bd945df744fce9519cf5eb190aacfed345e4bf66
chandanray917 <chandankumarsunilray@gmail.com>
2025-11-20 04:32:42 +0530

    summery of datatypes

---
+++ Languages_Learned/Javascript/9_dataTypeSummery/script.js
@@ -0,0 +1,51 @@
+// datatypes summery after two videos by ----chai aur code
+
+// primitive & non-primitive(reference_types)
+// primitive : 
+//  String
+//  Number
+//  Boolean
+//  Null
+//  Undefined
+//  Symboli
+//  BigInt
+
+// non-primitive(reference_types)
+// Array
+// Objects
+// Functions
+
+
+// dynamically typed language ------> javascript
+// statically typed language -------> java, python
+
+// primitive
+const score = 100
+const coreValue = 100.3
+const isLoggedIn = true
+const outSideTemp = null  // object(null is a object)
+let userEmail = undefined
+const id = Symbol("123")
+// const anotherId = Symbol('123')
+// console.log(id === anotherId);
+const bigInt = 32132151356464464546513513n;
+
+// non-primitive
+
+const ArreyHeros = ["shaktiman","naagraj","doga"];
+const Object={
+    name:"chandan",
+    age:22,
+
+}
+
+let books =  function notebook(){
+    console.log("hellow world");
+    
+}
+books()
+
+
+console.log(typeof books);
+
+
