4d7eafa44784e5cf58db14f0e9702b3e20d0ad0e
chandanray917 <chandankumarsunilray@gmail.com>
2025-11-20 00:55:13 +0530

    conversion of datatypes

---
+++ Languages_Learned/Javascript/6_dataConversion/script.js
@@ -0,0 +1,34 @@
+// let score = "33" ; 
+// let score = "33an" ; 
+// let score = null;
+let score = undefined;
+
+console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNUmber = Number(score);
+console.log(typeof (valueInNUmber));
+console.log(valueInNUmber);
+
+
+// "33=> 33"
+// "33abc=> NaN"
+// "ture" => 1 ; flase => 0
+
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn);
+
+//  1 => true; 0 => false
+// " " => false
+// "hitesh" => true
+
+
+
+// string conversion
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
