b0b06598ac0c90de61d0089f58fe029e9a78a2e7
chandanray917 <chandankumarsunilray@gmail.com>
2025-11-20 04:03:18 +0530

    string to number and some confusing calculations

---
+++ Languages_Learned/Javascript/7_stringToNumber_Converstion/script.js
@@ -0,0 +1,45 @@
+// =================operation==================
+// learn operator precidence
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = " hitesh";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// some confusing logic
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(3+4 * 5 % 3);
+
+// console.log(+true)
+console.log(+ "")
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+
+let gameCounter =  100 
+gameCounter++
+
+console.log(gameCounter);
+
+// prefix and postfix operator ------------------------------------------------------------------------------------must learn
+
+
+
+
+
+
+ 
