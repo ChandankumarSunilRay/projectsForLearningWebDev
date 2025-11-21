df0b8481f995f02bb15a59ad43d0124305c9075a
chandanray917 <chandankumarsunilray@gmail.com>
2025-11-21 06:11:23 +0530

    revised array

---
+++ Languages_Learned/Javascript/14_arrays/script.js
@@ -0,0 +1,35 @@
+// arrays(it is an object )
+// based on indexing
+// shallow copy
+// heap memory
+// 
+
+
+const myArr = [0, 1, 2, 3, 4, 5, true, "chandan"];
+const myHeros = ["shaktimam", "naagraj"]
+const myArr2 = new Array(1, 2, 3, 4, 5)
+
+//--------- array methods: 
+// myArr.push(6)
+// myArr.pop() // it removes last value
+// myArr.unshift(9)   // it insert something in 0'th index
+// myArr.shift()      // it removes first value
+// console.log(myArr.includes(2));   // find something in array , and can also ask index
+
+// const newArr = myArr.join()
+
+
+
+
+
+// console.log(newArr);
+
+
+console.log(myArr);   
+
+
+// slice and splice has to be revised again
+// slice don't delete data from array wheereas splice it always modifies the original array
+
+
+
