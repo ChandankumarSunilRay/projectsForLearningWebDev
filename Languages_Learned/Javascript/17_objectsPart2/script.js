c275ce5223073683ff4cac60cbbc686893c793fb
chandanray917 <chandankumarsunilray@gmail.com>
2025-11-23 23:50:53 +0530

    immediately invoked

---
+++ Languages_Learned/Javascript/17_objectsPart2/script.js
@@ -0,0 +1,18 @@
+const tinderUser = new Object()    // singleton object
+console.log(tinderUser);
+
+const secondTinderUser = {}       // non-singleton user
+
+
+
+
+// ------------question
+// var length = 20 ; 
+// function count(){
+//     console.log(this.length);
+   
+// }
+
+// const data = [ count , "A", 101]
+// data[0]("Mohit")
+
---
+++ Languages_Learned/Javascript/24_immediately_invoked_function/script.js
@@ -0,0 +1,15 @@
+//immediately invoked function (database connection)
+
+(function chai(){
+    // named iffi
+    console.log("database connection done");
+    
+})();
+
+// chai()
+
+
+((name)=>{
+    console.log(`Db connected  ${name}`);
+    
+})('paas_name');
