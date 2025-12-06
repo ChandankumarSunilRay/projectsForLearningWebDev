8b25159db0e0c555ca7d05e7ac197d2478928003
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-06 18:15:14 +0530

    added java keywords and data types

---
+++ Languages_Learned/java/1_Hellow_world/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld { // this is block
+    // one file can have multiple class but there will be only one class
+
+    public static void main(String[] args) { // psvm (shortcut for public static void main)
+        System.out.println("Hellow world ");
+        System.out.println(4 + 3);
+        
+
+    }
+
+}
---
+++ Languages_Learned/java/2_reserved_keywords/keywords.java
@@ -0,0 +1,87 @@
+/*
+    abstract
+    assert
+    boolean
+    break
+    byte
+    case
+    catch
+    char
+    class
+    const
+        continue
+        default
+        do
+        double
+        else
+        enum
+        extends
+        final
+        finally
+        float
+    for
+    goto
+    if
+    implements
+    import
+    instanceof
+    int
+    interface
+    long
+    native
+        new
+        package
+        private
+        protected
+        public
+        return
+        short
+        static
+        strictfp
+        super
+    switch
+    synchronized
+    this
+    throw
+    throws
+    transient
+    try
+    void
+    volatile
+    while
+
+
+
+*/
+
+/*
+    boolean 
+    byte  
+    short  
+    int
+    long
+    float
+    double
+    float
+    double
+    char
+
+*/
+public class keywords {
+
+    public static void main(String[] args) {
+        boolean isPassed = true;
+        int count = 12;
+        byte marks = 13;
+        float pi = 13.f;
+        char myLetter  = 'a' ;   // for char use only single quote ( ' ' ) 
+
+
+        System.out.println(count);
+        System.out.println(marks);
+        System.out.println(isPassed);
+        System.out.println(pi);
+        System.out.println(myLetter);
+
+    }
+}
