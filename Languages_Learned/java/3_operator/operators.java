2a97110c13e942eb677f7fda4c4db0ef826b72c4
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-07 22:47:21 +0530

    different types of operators are covered

--- Languages_Learned/java/3_operator/operators.java
+++ Languages_Learned/java/3_operator/operators.java
@@ -1,19 +1,79 @@
 public class operators {
 
     static void main(String[] args) {
+        // arithmetic operator
         int a = 12;
         int b = 3;
+
         int c = a + b;
         int d = a - b;
         int e = a * b;
-        int f = a / b;
-        int g = a % b;
+        int f = a / b; // it gives quotient
+        int g = a % b; // it gives remainder
 
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
         System.out.println(f);
         System.out.println(g);
+
+        /*
+        //  Assingment operator
+         * operator----Example----------Equivalent
+         * =  ------------- a = b ;-------     a=b;
+         * += ------------- a +=b ;---------   a=a+b ; 
+         * -= ------------- a-= b ;---------   a=a-b;
+         * =  ------------- a*=b  ;----------- a=a*b;
+         * /= ------------- a /=b ;----------  a=a/b;
+         * %= ------------- a%=b  ;----------- a=a%b;
+         * 
+         */
+
+        /*
+            // relational operator
+            ==  is Equal to 
+            !=  not Equal to 
+            >  Greater than
+            <  Smaller than 
+            >= Greater than with equal to 
+            <= smaller than with equal to
+
+        */
+
+        /*
+            logical operator
+            && AND
+            || OR
+            !  NOT
+
+        */
+
+        /*
+            Bitwise operator
+            ~   Biwise Complement
+            <<  Left shift
+            >>  Right shift
+            >>> Unsigned right shift
+            & Bitwise AND
+            ^ Bitwise exclusive OR
+
+        */
+
+        /*
+            increment    ++
+            decrement    --
+
+        */
+
+        /*
+            ternary operator-----
+        */
+
+        /*
+            Various Input Types using Scanner
+            --we can use nextLong(),nextFloat(),nextDouble(),and next() methods to get long, float, double, and string input respectively from the user.
+            -It is recommended to close the scanner object once the input is taken using the close() method
+        */
     }
 
 }
