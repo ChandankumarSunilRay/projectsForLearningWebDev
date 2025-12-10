3a006796e28049f7eb72be29ddf03af47079f0f2
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-10 23:56:24 +0530

    while loop added

--- Languages_Learned/java/7_loops/loops.java
+++ Languages_Learned/java/7_loops/loops.java
@@ -1,4 +1,5 @@
 public class loops {
+    // https://youtu.be/NNLoi8QqzaY?t=11372
     public static void main (String[] args){
         // ---common way--
         // System.out.println("Loops");
@@ -7,9 +8,19 @@ public class loops {
         // System.out.println("Loops");
         // System.out.println("Loops");
 
-        for(int i = 0; i<100; i++){       // initialize condition increment
+        // ---for loop
+        for(int i = 0; i<100; i++){          // initialize condition increment
             System.out.println("Loops");
+
         }
+
+        // ---wihle loops
+        // in while lool we don't know how many times we want to run loop (while condition don't fulfill keep running)
+        int i = 0 ; 
+        while (i < 5){
+            System.out.println(i);
+        }        
+
 
     }
 
