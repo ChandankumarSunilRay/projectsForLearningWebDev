a8c4dd167b1a7d34a4289896e78a29d81ce13e68
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-11 00:07:12 +0530

    do-while loop added

--- Languages_Learned/java/7_loops/loops.java
+++ Languages_Learned/java/7_loops/loops.java
@@ -1,5 +1,5 @@
 public class loops {
-    // https://youtu.be/NNLoi8QqzaY?t=11372
+    // https://youtu.be/NNLoi8QqzaY?t=12820
     public static void main (String[] args){
         // ---common way--
         // System.out.println("Loops");
@@ -16,11 +16,20 @@ public class loops {
 
         // ---wihle loops
         // in while lool we don't know how many times we want to run loop (while condition don't fulfill keep running)
-        int i = 0 ; 
+        int i = 1 ;   // infinte loop (bad practice) 
         while (i < 5){
             System.out.println(i);
         }        
 
+        // do-while loop
+        // A do-while loop is a control flow statement in programming that executes a block of code at least once, and then repeatedly executes it as long as a specified Boolean condition remains true.
+        int ii = 9;
+        do{
+            System.out.println(i);
+            ii++;
+        }while(ii <= 5);
+
+        
 
     }
 
