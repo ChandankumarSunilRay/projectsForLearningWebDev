5d4c3f37d0080c4d4adb620a4879a065f72be8fc
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-12 23:44:48 +0530

    first pattern printed

--- Languages_Learned/java/7_loops/nested_loops.java
+++ Languages_Learned/java/7_loops/nested_loops.java
@@ -2,16 +2,23 @@ public class nested_loops {
     public static void main(String[] args) {
         // if a loop exits inside the body of another looop, it's called a nested loop.
 
-        int i = 1;
+        // int i = 1;
 
-        for (int count = 0; count < 10; count++) {
-            for (i = 0; i < 6; i++) {
-                System.out.print(i + " ");
+        // for (int count = 0; count < 10; count++) {
+        //     for (i = 0; i < 6; i++) {
+        //         System.out.print(i + " ");
+        //     }
+        //     // System.out.println();
+        //     System.out.println("printed "+count);
+        // }
+
+                // printing pattern
+        for(int i=0; i<5; i++){
+            for(int j=0; j<i; j++){
+                System.out.print(j+" ");
             }
-            // System.out.println();
-            System.out.println("printed "+count);
+            System.out.println();
         }
-
 
 
 
