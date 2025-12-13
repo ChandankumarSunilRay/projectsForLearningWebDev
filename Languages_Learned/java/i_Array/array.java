af14c91ed36b39441cb0ff0b4f2ab168c4dce8b5
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-13 23:13:23 +0530

    array basics covered

---
+++ Languages_Learned/java/i_Array/array.java
@@ -0,0 +1,28 @@
+package Languages_Learned.java.i_Array;
+
+public class array {
+    public static void main(String[] args) {
+        // Array are stored in contiguous memory [consecutive memory locations]. array
+        // has fixed size
+        // both are valid declaration
+        // int intArray[];
+        // int[] intArray;
+
+        // intArray = new int[20]; //allocation memory to array
+
+        // int age[]; //declaration
+        // age = new int[5]; //allocation
+        int age[] = new int[5];
+        age[0] = 5;
+        age[1] = 2;
+
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+        System.out.println("length " + age.length);
+
+        int marks[] = { 54, 89, 56, 48 };
+        System.out.println(marks[2]);
+    }
+
+}
