6a92857488332687bae844748cd6a2760f389657
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-15 00:02:20 +0530

    added method/function

---
+++ Languages_Learned/java/j_methodsFunctions/methods.java
@@ -0,0 +1,14 @@
+package Languages_Learned.java.j_methodsFunctions;
+
+public class methods {
+    // https://youtu.be/NNLoi8QqzaY?t=21008
+    public static void main(String[] args) {
+        greet();
+    }
+
+    static void greet() {
+        System.out.println("Hello world");
+
+    }
+
+}
