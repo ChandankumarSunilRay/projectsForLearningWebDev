841b273c8a8d87fa7b5c11f3a69ceeb54d6cbf10
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-16 00:00:25 +0530

    started string

---
+++ Languages_Learned/java/j_string/string.java
@@ -0,0 +1,17 @@
+package Languages_Learned.java.j_string;
+
+public class string {
+    // https://youtu.be/NNLoi8QqzaY?t=23270
+    public static void main(String[] args) {
+        // 1. string literal
+        // 2. by new keyword
+
+        String name = "Anuj";
+        String sameName = "Anuj";
+        String newName = new String("Anuj");
+
+        System.out.println(name);
+        System.out.println(newName);
+
+    }
+}
