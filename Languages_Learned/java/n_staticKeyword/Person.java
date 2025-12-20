c54e06efff90a1112ff1afc68ff71c59930bb77e
chandanray917 <chandankumarsunilray@gmail.com>
2025-12-20 17:44:35 +0530

    static block

--- Languages_Learned/java/n_staticKeyword/Person.java
+++ Languages_Learned/java/n_staticKeyword/Person.java
@@ -3,5 +3,14 @@ package Languages_Learned.java.n_staticKeyword;
 public class Person {
     public static int count =12;
 
+    // this is static block
+    static{
+        // we can initialize the class etc before anything.
+        System.out.println("This is static block");
+    }
+
+    static{
+        System.out.println("static block runs at the first");
+    }
 
 }
