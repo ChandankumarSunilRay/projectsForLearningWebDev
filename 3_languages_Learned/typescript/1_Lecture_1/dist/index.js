e6e50544dd52f32a90b6838fd2023caf3d562df9
chandanray917 <chandankumarsunilray@gmail.com>
2026-01-26 11:30:37 +0530

    learning dom manipulation

---
+++ 3_languages_Learned/typescript/1_Lecture_1/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(person) {
+    return `Helo ${person}, Welcome to Chaicode`;
+}
+const username = "Chai aur Typescript";
+console.log(greet(username));
+console.log("just for test");
+//# sourceMappingURL=index.js.map
---
+++ 3_languages_Learned/typescript/1_Lecture_1/src/index.ts
@@ -0,0 +1,8 @@
+function greet(person: string): string {
+    return `Helo ${person}, Welcome to Chaicode`
+
+}
+const username: string = "Chai aur Typescript";
+console.log(greet(username));
+
+console.log("just for test");   
